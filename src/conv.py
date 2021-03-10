5021a9e0f02b03b9d63f6012ebc2779a27a86dc4
Thiago Silva <tsranzanidasilva@gmail.com>
2021-03-10 20:00:32 -0300

    Converted pdf pages to separate images.

--- src/conv.py
+++ src/conv.py
@@ -2,9 +2,9 @@
 from pdf2image import convert_from_path
 
 # Store Pdf with convert_from_path function
-images = convert_from_path('example.pdf')
+images = convert_from_path('path/est.pdf')
 
+# Save pages as images in the pdf
 for i in range(len(images)):
 
-      # Save pages as images in the pdf
     images[i].save('page'+ str(i) +'.jpg', 'JPEG')
