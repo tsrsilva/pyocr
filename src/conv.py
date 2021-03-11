3c9850eccae2736f794d517a6d3ba4a2db62c985
Thiago Silva <tsranzanidasilva@gmail.com>
2021-03-10 21:52:48 -0300

    Convertion of pdf to jpg added.

--- src/conv.py
+++ src/conv.py
@@ -1,10 +1,22 @@
-# import module
+# import modules
+import os
 from pdf2image import convert_from_path
 
+#detect the current working directory and print it
+path = os.getcwd()
+print ("The current working directory is %s" % path)
+input("Press enter to continue...")
+
+#detect the input and output directories and print it
+ipath = os.path.isdir("../input")
+print ("The existence of an input directory is %s" % ipath)
+input("Press enter to continue...")
+
+opath = os.path.isdir("../output")
+print ("The existence of an output directory is %s" % opath)
+input("Press enter to continue...")
+
 # Store Pdf with convert_from_path function
-images = convert_from_path('path/est.pdf')
-
-# Save pages as images in the pdf
-for i in range(len(images)):
-
-    images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+#try:
+images = convert_from_path('../input/est.pdf', output_folder = "../output",
+                            fmt = "JPEG", jpegopt = {"quality": 100})
