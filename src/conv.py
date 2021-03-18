f7f3caa1017097df9cdb08d2dbd6fdb2489147cd
Thiago Silva <tsranzanidasilva@gmail.com>
2021-03-18 19:33:36 -0300

    Adjusting files to comply to kite's rules.

--- src/conv.py
+++ src/conv.py
@@ -2,21 +2,21 @@
 import os
 from pdf2image import convert_from_path
 
-#detect the current working directory and print it
+# detect the current working directory and print it
 path = os.getcwd()
-print ("The current working directory is %s" % path)
+print("The current working directory is %s" % path)
 input("Press enter to continue...")
 
-#detect the input and output directories and print it
+# detect the input and output directories and print it
 ipath = os.path.isdir("../input")
-print ("The existence of an input directory is %s" % ipath)
+print("The existence of an input directory is %s" % ipath)
 input("Press enter to continue...")
 
 opath = os.path.isdir("../output")
-print ("The existence of an output directory is %s" % opath)
+print("The existence of an output directory is %s" % opath)
 input("Press enter to continue...")
 
 # Store Pdf with convert_from_path function
-#try:
-images = convert_from_path('../input/est.pdf', output_folder = "../output",
-                            fmt = "JPEG", jpegopt = {"quality": 100})
+# try:
+images = convert_from_path('../input/est.pdf', output_folder="../output",
+                           fmt="JPEG", jpegopt={"quality": 100})
--- src/ocr.py
+++ src/ocr.py
@@ -1,25 +1,29 @@
 import cv2
 import os
 
-#preprocessing modules
+# preprocessing modules
 import numpy as np
 from PIL import Image
 import glob
 
-#ocr modules
+# ocr modules
 import pytesseract
 
-#load images
-#cv_img = []
-#for img in glob.glob('../output/*.jpg'):
+# load images
+# cv_img = []
+# for img in glob.glob('../output/*.jpg'):
 #    n = cv2.imread(img)
 #    cv_img.append(n)
 
-#load test image
+# load test image
 img = cv2.imread('..output/test.jpg')
 
-#get grayscale image
+# Get grayscale image
+
+
 def get_grayscale(image):
+
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
 
 image.show("img")
