b18fb45be326ac39d38cd67f79dd088527e899c7
Thiago Silva <tsranzanidasilva@gmail.com>
2021-03-18 20:00:19 -0300

    Added ocr function

--- src/ocr.py
+++ src/ocr.py
@@ -18,11 +18,27 @@ import pytesseract
 # load test image
 image = cv2.imread('..output/test.jpg')
 
+# Adding custom options
+custom_config = r'--oem 3 --psm 6'
+pytesseract.image_to_string(image, config=custom_config)
+
 
 # Get grayscale image
 def get_grayscale(image):
-
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-image.show("image")
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+
+# thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255,
+                         cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
+# apply grayscale, noise removal and thresholding
+gray = get_grayscale(image)
+thresh = thresholding(gray)
