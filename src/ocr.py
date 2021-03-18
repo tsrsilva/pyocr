7537d6aca62095651750fa8db708a7fcf8130a3d
Thiago Silva <tsranzanidasilva@gmail.com>
2021-03-18 18:24:34 -0300

    First commit of ocr pipeline.

--- src/ocr.py
+++ src/ocr.py
@@ -1,2 +1,16 @@
 import cv2
+import os
+
+#preprocessing modules
+import numpy as np
+from PIL import Image
+import glob
+
+#ocr modules
 import pytesseract
+
+#load images
+cv_img = []
+for img in glob.glob('../output/*.jpg'):
+    n = cv2.imread(img)
+    cv_img.append(n)
