5ed8040d49ec9bf0c7ccf955c5a7803ed04c7060
Thiago Silva <tsranzanidasilva@gmail.com>
2021-03-18 19:13:08 -0300

    Preparing test

--- src/ocr.py
+++ src/ocr.py
@@ -10,7 +10,16 @@ import glob
 import pytesseract
 
 #load images
-cv_img = []
-for img in glob.glob('../output/*.jpg'):
-    n = cv2.imread(img)
-    cv_img.append(n)
+#cv_img = []
+#for img in glob.glob('../output/*.jpg'):
+#    n = cv2.imread(img)
+#    cv_img.append(n)
+
+#load test image
+img = cv2.imread('..output/test.jpg')
+
+#get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+image.show("img")
